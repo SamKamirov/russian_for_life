d8a19bf1071aeca7c085da88a826fc2cf891c8ac
sam <semkamirov@gmail.com>
2023-10-04 22:08:04 +0500

    Добавление функционала в хедер

--- js/scrollHeader.js
+++ js/scrollHeader.js
@@ -1,2 +1,24 @@
-var header = document.querySelector('.header')
-console.log(header);
+let title = document.querySelector('.post-single__title');
+let formatted = document.querySelector('.formatted')
+let clone = title.cloneNode(true);
+let cloneParent = document.querySelector('header .clone');
+let logo = document.querySelector('.logo')
+let header = document.querySelector('header .logo-link')
+
+const titlePosition = title.getBoundingClientRect().top;
+
+
+window.onscroll = () => {
+    if (window.scrollY > titlePosition) {
+        cloneParent.appendChild(clone);
+    }
+
+    else if (cloneParent.contains(clone)) {
+        cloneParent.removeChild(clone)
+        header.appendChild(logo)
+    }
+
+    if (window.scrollY > titlePosition && header.contains(logo) && window.outerWidth < 768) {
+        header.removeChild(logo);
+    }
+}
--- js/state.js
+++ js/state.js
@@ -1,6 +1,27 @@
 window.addEventListener('load', () => {
-    if (document.location.pathname != localStorage.getItem('location')) {
-        console.log('they"re not the same');
+    if (localStorage.getItem('location') === document.location.pathname) {
+        localStorage.setItem('last-visit', Date.now())
+        return
     }
+    
+    if (Number(Date.now()) - Number(localStorage.getItem('last-visit')) > 1200000)
+    {
+        document.querySelector('.notification-wrapper').classList.add('active')
+        localStorage.setItem('last-visit', Date.now());
+    }
+    
+    const notification = document.getElementById('notification-nav');
+    const link = document.createElement('a');
+    link.href = localStorage.getItem('location');
+    link.textContent = 'Да';
+    notification.appendChild(link);
+    
+    localStorage.setItem('last-visit', Date.now())
     localStorage.setItem('location', document.location.pathname);
+
+    document.querySelector('.notification-nav-button__decline').addEventListener('click', ()=> {
+        localStorage.setItem('last-visit', Date.now());
+        document.querySelector('.notification-wrapper').classList.remove('active')
+    })
 });
+
